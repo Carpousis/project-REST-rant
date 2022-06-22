9fe657278dafb7513187838ae029c768828668f4
Christopher Carpousis <bruceleeonidas@christophersair.myfiosgateway.com>
2022-06-22 18:13:20 -0400

    rendered 404 funtion in error page

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 require('dotenv').config()
-
 const express = require(`express`)
+const { engine } = require('express/lib/application')
 const app = express()
+
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
 
 app.use('/places', require('./controllers/places'))
 
 app.get('/', (_req, res) => {
-    res.send(`Hello World!`)
+    res.render(`Home`)
 })
 
 app.get('*', (_req, res) => {
-    res.status(404).send('<h1>404 Page</h1>')
-
+    res.render('error404')
 })
 
 app.listen(process.env.PORT)
