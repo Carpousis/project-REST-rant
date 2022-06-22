559fd424706c7e0dd3c357343e7d7348c48b0d17
Christopher Carpousis <bruceleeonidas@christophersair.myfiosgateway.com>
2022-06-22 17:03:42 -0400

    redid adding 404 page rout

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 require('dotenv').config()
+
 const express = require(`express`)
 const app = express()
 
 app.use('/places', require('./controllers/places'))
 
-app.get("/", (req, res) => {
+app.get('/', (_req, res) => {
     res.send(`Hello World!`)
 })
 
-app.get('*', (req, res) => {
+app.get('*', (_req, res) => {
     res.status(404).send('<h1>404 Page</h1>')
 
 })
